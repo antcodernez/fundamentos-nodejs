c5a0870ea91dc0f0736ce71f6e8c3c4c5853b9e6
Antonio Jiménez <marshday5@gmail.com>
2023-07-09 12:31:24 -0600

    Notes about env

---
+++ conceptos/entorno.js
@@ -0,0 +1,8 @@
+//Declaramos e inicializamos unas variables de entorno, sino estan definidas se usasra su valor por defecto que es el lado derecho del operador or
+let saludo = process.env.NOMBRE || "Pepe picas"; 
+let web = process.env.WEB || "No hay web :/"; 
+
+console.log(`Hola ${saludo}`);  
+console.log(`Mi web es:  ${web}`);  
+
+//Las variables de entorno se ponen en mayusculas y si es una palabra compuesta van con guion bajo ejemplos: NOMBRE, MI_NOMBRE.
